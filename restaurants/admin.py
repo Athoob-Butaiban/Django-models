fd385c56629f480f46607df9d673001845631635
athouba99 <athooba99@gmail.com>
2022-10-18 03:20:18 +0300

    done done

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Resttaurnt
+from .models import Restaurant
 
 # Register your models here.
 
-admin.site.register(Resttaurnt)
+admin.site.register(Restaurant)
--- restaurants/models.py
+++ restaurants/models.py
@@ -2,11 +2,11 @@ from email.policy import default
 from turtle import title
 from django.db import models
 
-class Resttaurnt(models.Model):
+class Restaurant(models.Model):
 
     name = models.CharField(max_length=30)
     description = models.TextField(default="") 
     opening_time = models.TimeField()
-    closing_time = models.TextField()
+    closing_time = models.TimeField()
     created_at = models.DateField(auto_now_add=True)
 
